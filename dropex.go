66cd17f81b9b9a1a5a318b57446b77e9f13805f4
Nemuel Wainaina <bR0S3@protonmail.com>
2023-03-21 11:54:31 +0300

    Add function to fetch payload

--- dropex.go
+++ dropex.go
@@ -5,11 +5,25 @@ import (
 	"net/http"
 )
 
+const (
+	HOME = ""
+)
+
 func main() {
 	fmt.Println("Hello friend")
+}
+
+func persist() {
+	
 }
 
 func has_internet_access() bool {
 	_, err := http.Get("https://www.google.com")
 	return err == nil
+}
+
+func fetch_payload() string {
+	resp, _ := http.Get(HOME)
+	fmt.Println(resp)
+	return ""
 }
